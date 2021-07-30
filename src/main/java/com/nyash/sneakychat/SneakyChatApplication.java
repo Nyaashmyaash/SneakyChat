33d34832d74a43e0ffcffb04f8ee2d888046dcb4
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-31 04:17:46 +0700

    Initial commit

---
+++ src/main/java/com/nyash/sneakychat/SneakyChatApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.sneakychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakyChatApplication.class, args);
+    }
+
+}

89637236790beeaaf304854c8c3e1774cb3db82a
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-31 05:57:46 +0700

    Add configurations

--- src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
+++ src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
@@ -1,38 +1,56 @@
 package com.nyash.sneakychat.api.ws;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.lang.NonNull;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Log4j2
 @ActiveProfiles("test-nyash")
 @RunWith(JUnitPlatform.class)
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
 public class WebSocketTests {
 
     @Value("${local.server.port}")
     private int port;
 
-    private static WebClient firstClient;
+    private static WebClient client;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @BeforeAll
     public void setup() throws Exception {
@@ -42,10 +60,32 @@ public class WebSocketTests {
         String wsUrl = "ws://127.0.0.1:" + port + "/ws";
 
         WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
+
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSession stompSession = stompClient
+                .connect(wsUrl, new StompSessionHandlerAdapter() {})
+                .get(1, TimeUnit.SECONDS);
+
+        client = WebClient.builder()
+                .stompClient(stompClient)
+                .stompSession(stompSession)
+                .handler(runStompFrameHandler)
+                .build();
+    }
+
+    @AfterAll
+    public void tearDown() {
+
+        if (client)
     }
 
     private List<Transport> createTransportClient() {
+        List<Transport> transports = new ArrayList<>(1);
 
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+
+        return transports;
     }
 
     @Data

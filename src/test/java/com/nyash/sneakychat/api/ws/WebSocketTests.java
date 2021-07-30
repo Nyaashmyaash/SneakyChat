88d3b9f045223ce4e52c486f8ed568e3f99f972f
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-31 05:37:56 +0700

    Add configurations

--- src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
+++ src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
@@ -1,9 +1,46 @@
 package com.nyash.sneakychat.api.ws;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @Log4j2
-@ActiveProfiles
+@ActiveProfiles("test-nyash")
+@RunWith(JUnitPlatform.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class WebSocketTests {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private static WebClient firstClient;
+
+    @Data
+    @Builder
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    private static class  WebClient {
+
+        WebSocketStompClient stompClient;
+
+        StompSession stompSession;
+
+        String sessionToken;
+
+        RunStompFrameHandler handler;
+    }
+
+    private static class RunStompFrameHandler{
+
+    }
 }

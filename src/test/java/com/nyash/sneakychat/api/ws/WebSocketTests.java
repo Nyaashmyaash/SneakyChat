9ab72b71c3ed966dec3e09c8bd436eae8a64f081
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-31 05:30:30 +0700

    Add configurations

---
+++ src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
@@ -0,0 +1,9 @@
+package com.nyash.sneakychat.api.ws;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.test.context.ActiveProfiles;
+
+@Log4j2
+@ActiveProfiles
+public class WebSocketTests {
+}

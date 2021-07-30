8ac558ca86e217a17dbca7d1c451aeb5fa37fe7d
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-31 05:48:35 +0700

    Add configurations

--- src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
+++ src/test/java/com/nyash/sneakychat/api/ws/WebSocketTests.java
@@ -1,18 +1,26 @@
 package com.nyash.sneakychat.api.ws;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Log4j2
 @ActiveProfiles("test-nyash")
@@ -25,6 +33,20 @@ public class WebSocketTests {
     private int port;
 
     private static WebClient firstClient;
+
+    @BeforeAll
+    public void setup() throws Exception {
+
+        RunStompFrameHandler runStompFrameHandler = new RunStompFrameHandler(new CompletableFuture<>());
+
+        String wsUrl = "ws://127.0.0.1:" + port + "/ws";
+
+        WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
+    }
+
+    private List<Transport> createTransportClient() {
+
+    }
 
     @Data
     @Builder
@@ -40,7 +62,29 @@ public class WebSocketTests {
         RunStompFrameHandler handler;
     }
 
-    private static class RunStompFrameHandler{
+    @Data
+    @AllArgsConstructor
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    private static class RunStompFrameHandler implements StompFrameHandler {
 
+        CompletableFuture<Object> future;
+
+        @Override
+        public @NonNull Type getPayloadType(StompHeaders stompHeaders) {
+
+            log.info(stompHeaders.toString());
+
+            return byte[].class;
+        }
+
+        @Override
+        public void handleFrame(@NonNull StompHeaders headers, Object o) {
+
+            log.info(o);
+
+            future.complete(o);
+
+            future = new CompletableFuture<>();
+        }
     }
 }

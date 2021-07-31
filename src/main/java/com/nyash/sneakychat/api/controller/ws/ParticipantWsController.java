dac7698d0c4972d0bb7e676398cbaeb772f85352
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-01 02:37:48 +0700

    Add changes

--- src/main/java/com/nyash/sneakychat/api/controller/ws/ParticipantWsController.java
+++ src/main/java/com/nyash/sneakychat/api/controller/ws/ParticipantWsController.java
@@ -1,13 +1,9 @@
 package com.nyash.sneakychat.api.controller.ws;
 
-import com.nyash.sneakychat.api.dto.MessageDto;
 import com.nyash.sneakychat.api.dto.ParticipantDto;
-import com.nyash.sneakychat.api.service.ParticipantService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
@@ -17,14 +13,14 @@ import org.springframework.stereotype.Controller;
 public class ParticipantWsController {
 
     public static final String FETCH_PARTICIPANT_JOIN_IN_CHAT = "/topic/chats.{chat_id}.participants.join";
-    public static final String FETCH_PARTICIPANT_LEAVE_CHAT = "/topic/chats.{chat_id}.participants.leave";
+    public static final String FETCH_PARTICIPANT_LEAVE_FROM_CHAT = "/topic/chats.{chat_id}.participants.leave";
 
     @SubscribeMapping(FETCH_PARTICIPANT_JOIN_IN_CHAT)
     public ParticipantDto fetchParticipantJoinInChat() {
         return null;
     }
 
-    @SubscribeMapping(FETCH_PARTICIPANT_LEAVE_CHAT)
+    @SubscribeMapping(FETCH_PARTICIPANT_LEAVE_FROM_CHAT)
     public ParticipantDto fetchParticipantLeaveChat() {
         return null;
     }
@@ -34,7 +30,7 @@ public class ParticipantWsController {
     }
 
     public String getFetchParticipantLeaveChat(String chatId) {
-        return FETCH_PARTICIPANT_LEAVE_CHAT.replace("{chat_id}", chatId);
+        return FETCH_PARTICIPANT_LEAVE_FROM_CHAT.replace("{chat_id}", chatId);
     }
 
 }

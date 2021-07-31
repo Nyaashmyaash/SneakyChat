e64a6a52ee1006a3f3f5dd33647efd080d732cba
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-31 09:45:21 +0700

    Add changes

---
+++ src/main/java/com/nyash/sneakychat/api/service/ParticipantService.java
@@ -0,0 +1,14 @@
+package com.nyash.sneakychat.api.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Service
+public class ParticipantService {
+
+
+}

41974ab05fe66993c12e43b1dadbe5ba79fdf3a6
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-01 07:35:13 +0700

    Add changes

--- src/main/java/com/nyash/sneakychat/api/service/ParticipantService.java
+++ src/main/java/com/nyash/sneakychat/api/service/ParticipantService.java
@@ -6,13 +6,23 @@ import com.nyash.sneakychat.api.dto.ParticipantDto;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.SetOperations;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.messaging.AbstractSubProtocolEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
+@Log4j2
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service
@@ -20,7 +30,7 @@ public class ParticipantService {
 
     SimpMessagingTemplate messagingTemplate;
 
-    private static final Map<String, Participant> participantMap = new ConcurrentHashMap<>();
+    private static final Map<String, Participant> sessionIdToParticipantMap = new ConcurrentHashMap<>();
 
     SetOperations<String, Participant> setOperations;
 
@@ -32,7 +42,7 @@ public class ParticipantService {
                 .chatId(chatId)
                 .build();
 
-        participantMap.put(participant.getId(), participant);
+        sessionIdToParticipantMap.put(participant.getId(), participant);
 
         setOperations.add(ParticipantKeyHelper.makeKey(chatId), participant);
 
@@ -43,6 +53,46 @@ public class ParticipantService {
                         .enterAt(participant.getEnterAt())
                         .build()
         );
+    }
+
+    public void handleUnsubscribe(SessionUnsubscribeEvent event) {
+        handleLeaveChat(event);
+    }
+
+    public void handleDisconnect(SessionDisconnectEvent event) {
+        handleLeaveChat(event);
+    }
+
+    private void handleLeaveChat(AbstractSubProtocolEvent event) {
+
+        final SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+
+        Optional
+                .ofNullable(headerAccessor.getSessionId())
+                .map(sessionIdToParticipantMap::remove)
+                .ifPresent(participant -> {
+
+                    setOperations.remove(
+                            ParticipantKeyHelper.makeKey(participant.getChatId()),
+                            participant
+                    );
+
+                    messagingTemplate.convertAndSend(
+                            ParticipantWsController.getFetchParticipantLeaveChat(participant.getChatId()),
+                            ParticipantDto.builder()
+                                    .id(participant.getId())
+                                    .enterAt(participant.getEnterAt())
+                                    .build()
+                    );
+                });
+
+    }
+
+    public Stream<Participant> getParticipants(String chatId) {
+        return Optional
+                .ofNullable(setOperations.members(ParticipantKeyHelper.makeKey(chatId)))
+                .orElseGet(HashSet::new)
+                .stream();
     }
 
     private static class ParticipantKeyHelper {

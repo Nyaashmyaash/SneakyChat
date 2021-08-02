1d5a60588cd8c2f58b2e83a16523100b15d16594
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-02 22:07:59 +0700

    Add changes

--- src/main/java/com/nyash/sneakychat/api/service/ChatService.java
+++ src/main/java/com/nyash/sneakychat/api/service/ChatService.java
@@ -1,7 +1,11 @@
 package com.nyash.sneakychat.api.service;
 
+import com.nyash.sneakychat.api.domain.Chat;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
+
+    SetOperations<String, Chat> setOperations;
 }
